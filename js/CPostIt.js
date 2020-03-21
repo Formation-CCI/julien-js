3f572dca07be5312e465540bde56cab5ce612e41
Cedric <paladicecorporation@gmail.com>
2020-03-21 14:14:12 +0100

    Structure + modif taille postit

--- js/CPostIt.js
+++ js/CPostIt.js
@@ -3,22 +3,46 @@ let x;
 let y;
 let i = 0;
 let onBouge = false;
+let onTaille = false;
+
+/* Importation des éléments HMTL */
+var postit1 = document.querySelector(".post-1");
+var postit2 = document.querySelector(".post-2");
+var postit3 = document.querySelector(".post-3");
+var zone = document.querySelector(".post-js");
+
+/* Position souris dans la page */
+document.addEventListener('mousemove', function souris(event)
+{
+    x = event.clientX;
+    y = event.clientY;
+});
+
+/* Lorsque l'on relache la souris */
+document.addEventListener('mouseup', () => {
+    onBouge = false;
+    onTaille = false;
+})
 
 /* Objet Postit */
 class PostIt 
 {
     posX;
     posY;
+    hauteur;
+    largeur;
     couleurTexte;
     couleurBackground;
 
-    constructor(posX, posY, couleurTexte, couleurBackground, numPostIt) 
+    constructor(posX, posY, hauteur, largeur, couleurTexte, couleurBackground, numPostIt) 
     {
         this.posX = posX;
         this.posY = posY;
-        this.numPostIt = numPostIt;
+        this.hauteur = hauteur;
+        this.largeur = largeur;
         this.couleurTexte = couleurTexte;
         this.couleurBackground = couleurBackground;
+        this.numPostIt = numPostIt;
     }
 
     changePlace(x, y) 
@@ -27,22 +51,27 @@ class PostIt
         this.posY = y;
     }
 
+    changeTaille(x, y) 
+    {
+        this.hauteur = y;
+        this.largeur = x;
+    }
+
     affichePostit() 
     {
         let postit;
+        let buttonMove;
+        let buttonTaille;
         let creation = false;
 
         // Si le Postit n'existe pas on le crée
         if (document.getElementById('postIt') == null)
         {
-            console.log(document.getElementById('postIt'));
             postit = document.createElement('div');
             creation = true;
         }
         else
-        {
             postit = document.getElementById('postIt');
-        }
 
         // Paramétrage du Postit
         postit.id = "postIt";
@@ -50,38 +79,40 @@ class PostIt
         postit.style.position = "fixed";
         postit.style.top = this.posY + "px";
         postit.style.left = this.posX + "px";
-        postit.style.width = "150px";
-        postit.style.height = "150px";
+        postit.style.height = this.hauteur + "px";
+        postit.style.width = this.largeur + "px";
         postit.style.color = this.couleurTexte;
         postit.style.backgroundColor = this.couleurBackground;
         postit.style.padding = "2em"; 
         postit.innerHTML = "<p>Je suis un objet de test</p>";
-
+        
         // Ajout du Postit dans la page 
         if(creation)
         {
             zone.appendChild(postit);
         }
 
-        // Action lorsqu'on clique sur le postit
-        postit.addEventListener('click', () => {
+        // Paramétrage de buttonMove
+        buttonMove = document.createElement('button');
+        buttonMove.textContent = "Bouger";
+        postit.appendChild(buttonMove);
+
+        // Paramétrage de buttonTaille
+        buttonTaille = document.createElement('button');
+        buttonTaille.textContent = "Taille";
+        postit.appendChild(buttonTaille);
+
+        // Action lorsqu'on clique sur buttonMove
+        buttonMove.addEventListener('mousedown', () => {
             onBouge = true;
+        })
+
+        // Action lorsqu'on clique sur le buttonTaille
+        buttonTaille.addEventListener('mousedown', () => {
+            onTaille = true;
         })
     }
 }
-
-/* Importation des éléments HMTL + Position souris dans la page */
-
-var postit1 = document.querySelector(".post-1");
-var postit2 = document.querySelector(".post-2");
-var postit3 = document.querySelector(".post-3");
-var zone = document.querySelector(".post-js");
-
-document.addEventListener('mousemove', function souris(event)
-{
-    x = event.clientX;
-    y = event.clientY;
-});
 
 /* Création des postits */
 /*
@@ -113,22 +144,23 @@ postit3.addEventListener('click', function()
         });
 }); */
 
-monTest = new PostIt(500, 500, "black", "rgb(255,215,7)", i);
-monTest.affichePostit();
-
-
-/* Faire bouger un Postit */
-
-document.addEventListener('mouseup', () => {
-    onBouge = false;
-})
-
 function refresh() {
+
+    // Bouger le postit
     if (onBouge) {
         monTest.changePlace(x, y);
         monTest.affichePostit();
     }
+
+    // Modifier la taille du postit
+    if (onTaille) {
+        monTest.changeTaille(x, y);
+        monTest.affichePostit();
+    }
+
     setTimeout(refresh, 50)
 }
-
 refresh();
+
+monTest = new PostIt(400, 300, 242, 242, "black", "rgb(255,215,7)", i);
+monTest.affichePostit();
